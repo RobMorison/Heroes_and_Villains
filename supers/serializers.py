454038e690b6b48ff3e5113e0424c75c2277f432
Sara Morison <rob.morison32@gmail.com>
2023-01-19 13:20:59 -0500

    Added put functionality

--- supers/serializers.py
+++ supers/serializers.py
@@ -7,4 +7,4 @@ class SupersSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_types', 'super_types_id']
         depth = 1
 
-    super_types_id = serializers.IntegerField(write_only=True)
+   
