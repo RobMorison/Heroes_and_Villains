babc4f19979099ae1de98c487e42d78724dac80a
Sara Morison <rob.morison32@gmail.com>
2023-01-24 12:48:52 -0500

    Added Postman import

--- supers/views.py
+++ supers/views.py
@@ -4,16 +4,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SupersSerializer
 from .models import Supers
+from super_types.models import SuperTypes
+from super_types.serializers import SuperTypesSerializer
 
 # Create your views here.
+
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
 
         super_type = request.query_params.get('type')
-        print(super_type)
-
+   
         queryset = Supers.objects.all()
 
         if super_type:
@@ -21,12 +23,16 @@ def supers_list(request):
 
         serializer = SupersSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
+
+
     elif request.method == 'POST':
         serializer = SupersSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
